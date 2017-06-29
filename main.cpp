749741d3c5829932b44e5d12a9acdb5552ffb5e2
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-06-30 00:04:39 +0200

    S

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    using namespace std;
+    
+    cout << "hello" << endl;
+    cout << "hello again";
+    cout << endl;
+
+    return 0;
+}
+
