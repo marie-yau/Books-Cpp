ac8f6dea127bf10f488b5e7591742da41829f128
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-07-07 03:15:55 +0200

    Add files via upload

---
+++ C PlusPlus Primer Plus/Unit 04/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "What is your first name? ";
+    string firstName;
+    getline(cin, firstName);
+    cout << "What is your last name? ";
+    string lastName;
+    getline(cin, lastName);
+    enum grades {A, B, C};
+    cout << "What letter grade do you deserve? ";
+    grades deservedGrade;
+    cin >> deservedGrade;
+    cout << "What is your age? ";
+    int age;
+    cin >> age;
+    cout << "Name: " << lastName << ", " << firstName << endl;
+    cout << "Grade: " << deservedGrade + 1 << endl;
+    cout << "Age: " << age << endl;
+
+    return 0;
+}
+
