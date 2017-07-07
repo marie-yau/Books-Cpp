2e0ac48079932a5a7dc232c65e1f884573d09a62
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-07-07 03:14:51 +0200

    Add files via upload

---
+++ C PlusPlus Primer Plus/Unit 04/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    double heights[2] = {140.3, 180.1};
+    int ages [2] = {10, 26};
+    
+    double * p_heights = heights;
+    int * p_ages = &ages[0];
+    
+    cout << "height[0] = " << heights[0] << endl;
+    cout << "height[1] = " << *(p_heights + 1) << endl;
+
+    return 0;
+}
+
