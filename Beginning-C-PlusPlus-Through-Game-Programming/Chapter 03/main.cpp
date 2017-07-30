d9c1322acb3552d3628cd7d2671120e450a8e9b7
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-07-30 11:56:43 +0200

    Add files via upload

---
+++ Beginning-C-PlusPlus-Through-Game-Programming/Chapter 03/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    const int ROWS = 2; 
+    const int COLUMNS = 3;
+    char board[ROWS][COLUMNS] = { {'O', 'X', 'O'}, {'\n', 'X', 'X'} };
+
+    return 0;
+}
+
