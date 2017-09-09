3fb3479596657c7d6e6626b934984b092856fa71
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-09-09 14:17:52 -0700

    Add files via upload

---
+++ Problem Solving with C PlusPlus/Chapter 01/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "***********  \n";
+    cout << "  CC   SSS   \n";
+    cout << " C  C  S     \n";
+    cout << " C     SSS   \n";
+    cout << " C  C    S   \n";
+    cout << "  CC   SSS   \n";
+    cout << "***********  \n";
+    return 0;
+}
+
