6e3a5bea4968037a36b6c1273c12aecbdcc66e37
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-12-01 04:37:56 -0800

    Memory Models and Namespaces

---
+++ C PlusPlus Primer Plus/Unit 09/E4/sales.h
@@ -0,0 +1,21 @@
+#pragma once  
+/*#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::setw;
+using std::cin;*/
+
+namespace SALES{
+    const int QUARTERS = 4;
+    struct Sales{
+        double sales[QUARTERS];
+        double average;
+        double max;
+        double min;
+    };
+    
+    void setSales(Sales & s, const double ar[], int n);
+    void setSales(Sales & s);
+    void showSales(const Sales & s);
+}
