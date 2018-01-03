aeee607274ca4208bd466d07d3b769ea3f00cc63
GeekTheGreat <marie.dohnalova21@gmail.com>
2018-01-03 00:08:05 -0800

    Objects and Classes

---
+++ C PlusPlus Primer Plus/Unit 10/E7/Plorg.h
@@ -0,0 +1,15 @@
+#ifndef PLORG_H
+#define PLORG_H
+
+class Plorg {
+public:
+    Plorg();
+    void setCi(int c);
+    void showPlorg();
+private:
+    char name[20];
+    int ci;
+};
+
+#endif /* PLORG_H */
+
