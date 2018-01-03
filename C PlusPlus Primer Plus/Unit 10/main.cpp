cb298f1738a2240a9d04184fac855030a655b700
GeekTheGreat <marie.dohnalova21@gmail.com>
2018-01-03 00:06:59 -0800

    Add files via upload

---
+++ C PlusPlus Primer Plus/Unit 10/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+
+#include "Plorg.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    Plorg p;
+    p.showPlorg();
+    p.setCi(33);
+    p.showPlorg();
+
+    return 0;
+}
+
