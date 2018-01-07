932029ed1b1123cd2e11849549282df5eb783b4c
GeekTheGreat <marie.dohnalova21@gmail.com>
2018-01-07 04:13:35 -0800

    Working with Classes

---
+++ C PlusPlus Primer Plus/Unit 11/E2/Vector.h
@@ -0,0 +1,35 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <iostream>
+
+namespace VECTOR{
+    class Vector {
+    public:
+        Vector();
+        Vector(double n1, double n2, char form = 'r');
+        void set(double n1, double n2, char form = 'r');
+        ~Vector();
+        double xval() const {return x;};
+        double yval() const {return y;};
+        double magval() const;
+        double angval() const;
+        void polar_mode();
+        void rect_mode();
+        //operator overloading
+        Vector operator+(const Vector &b) const;
+        Vector operator-(const Vector &b) const;
+        Vector operator-() const;
+        Vector operator*(double n) const;
+        //friends
+        friend Vector operator*(double n, const Vector &a);
+        friend std::ostream & operator<<(std::ostream & os, const Vector &v);
+    private:
+        double x;
+        double y;
+        char mode;
+        void set_x(double mag, double ang);
+        void set_y(double mag, double ang);
+    };
+}
+#endif
+
