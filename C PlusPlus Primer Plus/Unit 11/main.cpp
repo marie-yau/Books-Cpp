528ab4df000a658c567a28586e306edc415ba039
GeekTheGreat <marie.dohnalova21@gmail.com>
2018-01-07 04:12:50 -0800

    Add files via upload

---
+++ C PlusPlus Primer Plus/Unit 11/main.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
+#include "Vector.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    using namespace std;
+    using VECTOR::Vector;
+    
+    srand(time(0));
+    double direction;
+    Vector step;
+    Vector result(0.0, 0.0);
+    unsigned long steps = 0;
+    double target;
+    double dstep;
+    
+    cout << "Enter target distance: ";
+    cin >> target;
+    cout << "Enter step length: ";
+    cin >> dstep;
+    
+    cout << "Target Distance: " << target << ", Step Size: " << dstep << endl;
+    cout << steps << ": " << result;
+    while(result.magval() < target){
+        direction = rand() % 360;
+        step.set(dstep, direction, 'p');
+        result = result + step;
+        steps++;
+        cout << "\n" << steps << ": " << result;
+    } 
+    cout << "\nAfter " << steps << " steps, the subject has the following location: " << endl;
+    cout << result;
+    result.polar_mode();
+    cout << result;
+    
+    return 0;
+}
